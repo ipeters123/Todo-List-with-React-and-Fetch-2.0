8fc42c63da574ce5f86cffca27b7ec04893d939d
Piero Antonucci Fernández <piero.antonucci@gmail.com>
2021-03-07 23:13:40 -0300

    Todo list almost finished

--- src/components/formtodo.jsx
+++ src/components/formtodo.jsx
@@ -1,18 +1,73 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+
+// Crear funcion que cree el usuario, este se usa una sola vez y luego no se vuelve a llamar mas
+
+function CreateUser(){
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/piero',{
+        method: 'POST',
+        body: '[]',
+        headers : {
+            'Content-Type':'application/json'
+    }})
+    .then(res => res.json())
+    .then(response => console.log('Success', JSON.stringify(response)))
+    .catch(error => console.log('Error', error));
+}
 
 export const FormToDo = (props) => {
-    // Crear funcion que cree el usuario
+        
     // Crear funcion que importe las tareas desde la API
-
+    
     const[list, setList]=useState([]);
     const[task, setTask]=useState({});
-    console.log(list)
+
+    async function GetTaskFromList(){
+        const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/piero',{
+            method : 'GET',
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        })
+        const data = await response.json();
+        console.log(data);
+        setList(data);
+    }
+    
+    async function AddTaskToList(props){
+        await fetch('https://assets.breatheco.de/apis/fake/todos/user/piero',{
+            method : 'PUT',
+            body: JSON.stringify(props),
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        })
+    }
+
+    async function DeleteAllTasks(){
+        await fetch('https://assets.breatheco.de/apis/fake/todos/user/piero',{
+            method : 'PUT',
+            body: '[]',
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        })
+    }
+
+    useEffect(
+        ()=>{
+            CreateUser();
+            GetTaskFromList();
+        },[]
+    )
     return(
         <>  <div className="container">
-                <form onSubmit={(event) => {list.push(task); setTask({label:""}); event.preventDefault()}}>
+                <span>{JSON.stringify(task)}</span>
+                <span>{JSON.stringify(list)}</span>
+                <form onSubmit={(event) => {list.push(task); setTask({label:""}); event.preventDefault(); AddTaskToList(list)}}>
                 <input type="text" value={task.label} className="input" onChange={(event) => {setTask({label: event.target.value, done: false})}} placeholder="Ingresa tu Tarea"/>
             </form>
             <div className="task-list">
@@ -24,7 +79,7 @@ export const FormToDo = (props) => {
                             <li key={index} className="list-group-item">
                                 {item.label}
                             <a>
-                            <FontAwesomeIcon icon={faTrashAlt} color="white" className="trashIcon" onClick={()=> setList(list.filter((i) => i !==item))}/>    
+                            <FontAwesomeIcon icon={faTrashAlt} color="white" className="trashIcon" onClick={() => {setList(list.filter((i) => i !==item))}}/>    
                                 </a>
                             </li>
                         );
@@ -32,6 +87,14 @@ export const FormToDo = (props) => {
                      )    
                     }
                 </ul>
+            </div>
+            <hr></hr>
+            <div>Delete All   
+            <FontAwesomeIcon icon={faTrashAlt} color="white" className="trashIcon" onClick={()=> {setList([]); DeleteAllTasks()}}/>
+            </div>
+            <hr></hr>
+            <div>
+            <Button className="deleteButton" variant="dark" onClick={() => AddTaskToList(list)}>Presiona aqui al eliminar una tarea</Button>
             </div>
             <hr></hr>
             <div>
